d32eb1f15789e9ca325a90b7a9d8da29385123ef
Ticola <reynvmv.4353@gmail.com>
2023-11-06 02:57:29 -0600

    Update main text content

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,9 +2,9 @@
 const MainContent = () => {
   return (
     <div className="mt-16 mb-8 mx-auto max-w-2xl p-4">
-      <h1 className="text-5xl font-bold text-center text-slate-800 mb-6">Rapidly decode the visual stories of any website.</h1>
+      <h1 className="text-5xl font-bold text-center text-slate-800 mb-6">Unlock the story behind every image</h1>
       <p className="text-xl text-gray-700 text-center">
-        A sophisticated web scraping tool designed to efficiently extract and catalog alt text from images across the web.
+      Transform how you collect web imagery data with our specialized scraper, built to adeptly isolate and organize alt text, streamlining your workflow and enhancing content discovery.
       </p>
     </div>
   );
