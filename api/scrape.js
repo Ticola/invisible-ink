a38685c1e761719b74b81ad80de0beff1d465a66
Ticola <reynvmv.4353@gmail.com>
2023-11-07 03:24:40 -0600

    Update logic to fetch all img elements as well as picture elements and avoid duplicates

--- api/scrape.js
+++ api/scrape.js
@@ -19,6 +19,7 @@ module.exports = async (req, res) => {
     const $ = cheerio.load(html);
     const altTexts = [];
     const processedSrcs = new Set(); // To keep track of processed src values
+    const excludedSelectors = '.cmp-experiencefragment--header picture, .cmp-experiencefragment--Header picture, .cmp-experiencefragment--footer picture, .cmp-experiencefragment--whirlpool-meganav picture';
 
     const getBaseUrl = (inputUrl) => {
       const urlObject = new URL(inputUrl);
@@ -27,7 +28,7 @@ module.exports = async (req, res) => {
 
     const baseUrl = getBaseUrl(url);
 
-    $('picture').not('.cmp-experiencefragment--header picture, .cmp-experiencefragment--Header picture, .cmp-experiencefragment--footer picture, .cmp-experiencefragment--whirlpool-meganav picture').each((i, pictureElem) => {
+    $('picture').not(excludedSelectors).each((i, pictureElem) => {
       const imgElem = $(pictureElem).find('img');
       let src = imgElem.attr('src') || imgElem.attr('data-src') || '';
       let alt = imgElem.attr('alt') || '[No Alt Text]';
@@ -43,12 +44,26 @@ module.exports = async (req, res) => {
       }
 
       if (src && !processedSrcs.has(src)) { // Check if this src has not been processed before
-        if (!src.match(/^https?:\/\/.+\/.+/)) {
-          src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png";
-        }
+        processedSrcs.add(src); // Add src to processedSrcs to avoid duplicates later
+        altTexts.push({ src, alt });
+      }
+    });
+
+    $('img').not('picture img').each((i, imgElem) => { // Select img elements that are not inside picture elements
+      let src = $(imgElem).attr('src') || $(imgElem).attr('data-src') || '';
+      let alt = $(imgElem).attr('alt') || '[No Alt Text]';
+
+      if (src && !src.startsWith('http://') && !src.startsWith('https://')) {
+        src = new URL(src, baseUrl).href;
       }
 
-      altTexts.push({ src, alt });
+      if (src && !processedSrcs.has(src) && src.match(/^https?:\/\/.+\/.+/)) {
+        processedSrcs.add(src); // Add src to processedSrcs to track it
+        altTexts.push({ src, alt }); // Push the image information
+      } else if (!src.match(/^https?:\/\/.+\/.+/)) {
+        src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png";
+        altTexts.push({ src, alt }); // Push the placeholder image information
+      }
     });
 
     res.json({ altTexts });
