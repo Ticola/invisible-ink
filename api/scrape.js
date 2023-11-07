4a70811fbc30731c567767a16bb54ddc89cd47db
Ticola <reynvmv.4353@gmail.com>
2023-11-07 02:12:29 -0600

    Update scraping logic to handle both src and srcset in a single pass

--- api/scrape.js
+++ api/scrape.js
@@ -25,27 +25,33 @@ module.exports = async (req, res) => {
     };
 
     const baseUrl = getBaseUrl(url);
+    
+    const images = $('img').not('.cmp-experiencefragment--header img, .cmp-experiencefragment--Header img, .cmp-experiencefragment--footer img, .cmp-experiencefragment--whirlpool-meganav img');
 
-    $('img').not('.header img, .footer img, .cmp-experiencefragment--header img, .cmp-experiencefragment--Header img, .cmp-experiencefragment--footer img, .cmp-experiencefragment--whirlpool-meganav img').each((i, elem) => {
-      let src = $(elem).attr('src') || '';
-      let alt = $(elem).attr('alt') || '[No Alt Text]';
-      let srcset = $(elem).closest('picture').find('source').attr('srcset') || '';
+    images.each((i, elem) => {
+      // Determine the image source. Prefer 'src' over 'data-src', and 'srcset' over 'data-srcset' when available
+      let src = $(elem).attr('src') || $(elem).attr('data-src') || '';
+      let srcset = $(elem).attr('srcset') || $(elem).attr('data-srcset') || '';
 
-      // Use the srcset if it's available and the src doesn't have a valid URL
-      if (!src.match(/^https?:\/\//) && srcset) {
-        src = srcset.split(',').map(item => item.trim().split(' ')[0])[0];
+      // If the image is within a .flyout-img, attempt to use the srcset
+      if ($(elem).parents('.flyout-img').length && srcset) {
+        src = srcset.split(',')[0].trim().split(' ')[0];
       }
 
-      // If src is not a full URL, prepend the base URL
-      if (src && !src.match(/^https?:\/\//)) {
+      // If src is a relative URL, prepend the base URL
+      if (src && !src.startsWith('http://') && !src.startsWith('https://')) {
         src = new URL(src, baseUrl).href;
       }
 
       // If src is still not a valid URL, use placeholder
-      if (!src.match(/^https?:\/\/.+\/.+/)) {
+      if (!src || !src.match(/^https?:\/\/.+\/.+/)) {
         src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png";
       }
 
+      // Get alt text
+      const alt = $(elem).attr('alt') || '[No Alt Text]';
+      
+      // Push the src and alt into the altTexts array
       altTexts.push({ src, alt });
     });
 
